35b82ec38e95b52ccc13b8dd8c5605ca8e346e23
siw36 <rhh.klussmann@gmail.com>
2019-06-24 19:15:48 +0200

    nodocs and entrypoint

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM centos:latest
 
 COPY entrypoint.sh /entrypoint.sh
 
-RUN yum -y install —-no-docs dd hdparm fpaste \
+RUN yum -y install --nodocs dd hdparm fpaste \
   && chown 0:0 /entrypoint.sh \
   && chmod 770 /entrypoint.sh
 
---
+++ entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+echo "Starting performance data collection"
+echo "-------------------------------------"
+
+echo $(date) > /tmp/result.txt
+echo "Testing write performance"
+echo "-------------------------------------"
+echo "Write performance:" > /tmp/result.txt
+echo "-------------------------------------" > /tmp/result.txt
+dd if=/dev/zero of=/mnt/pvc/test-io bs=1G count=1 oflag=dsync 2> /tmp/result.txt
+
+echo "Testing read performance"
+echo "-------------------------------------"
+echo "Read performance:" > /tmp/result.txt
+echo "-------------------------------------" > /tmp/result.txt
+dd if=/mnt/pvc/test-io of=/dev/null bs=8k 2> /tmp/result.txt
+
+echo "Testing latency"
+echo "-------------------------------------"
+echo "Latency:" > /tmp/result.txt
+echo "-------------------------------------" > /tmp/result.txt
+dd if=/dev/zero of=/mnt/pvc/test-latency bs=512 count=1000 oflag=dsync 2> /tmp/result.txt
+
+echo "Creating result"
+echo "-------------------------------------"
+fpaste /tmp/result.txt
+
+echo This container will sleep for 5 minutes and then gets terminated
+sleep 300
+exit 0
